abf7335ad53590d4d9429fe536d4d17a8d7a1907
Corey <corey@coreypon.com>
2015-04-25 10:58:01 -0500

    Seperated out the test clases for the event schedule and the scheduled event, thanks for the well named types microsoft

--- WMCDuplicateRemover.Tests/EventScheduleTests.cs
+++ WMCDuplicateRemover.Tests/EventScheduleTests.cs
@@ -8,24 +8,15 @@ using Microsoft.MediaCenter.TV.Scheduling;
 
 namespace WMCDuplicateRemover.Tests
 {
+    [Ignore]
     [TestFixture]
-    public class WMCDuplicateRemoverTests
+    public class EventScheduleTests
     {
-        IScheduledEvent _scheduledEvent;
         IEventSchedule _eventSchedule;
 
         [SetUp]
         public void SetUp()
         {
-            _scheduledEvent = new MockScheduledEvent(
-                "The Daily Show", 
-                "Jon Stewart Discusses Current Events", 
-                false, 
-                new DateTime(2015, 3, 23, 10, 0, 0), 
-                false, 
-                new DateTime(2015, 3, 23, 10, 0, 0), 
-                Microsoft.MediaCenter.TV.Scheduling.ScheduleEventStates.WillOccur);
-
             _eventSchedule = new MockEventSchedule(
                 new List<IScheduledEvent>
                 {
@@ -143,18 +134,6 @@ namespace WMCDuplicateRemover.Tests
                         ScheduleEventStates.WillOccur)
                 }
                 );
-        }
-
-        [Test]
-        public void TestCreateScheduledEvent()
-        {
-            Assert.AreEqual("The Daily Show", _scheduledEvent.Title);
-            Assert.AreEqual("Jon Stewart Discusses Current Events", _scheduledEvent.Description);
-            Assert.False(_scheduledEvent.Partial);
-            Assert.AreEqual(new DateTime(2015, 3, 23, 10, 0, 0), _scheduledEvent.OriginalAirDate);
-            Assert.False(_scheduledEvent.Repeat);
-            Assert.AreEqual(new DateTime(2015, 3, 23, 10, 0, 0), _scheduledEvent.StartTime);
-            Assert.AreEqual(ScheduleEventStates.WillOccur, _scheduledEvent.State);
         }
     }
 }
---
+++ WMCDuplicateRemover.Tests/ScheduledEventTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+using Microsoft.MediaCenter.TV.Scheduling;
+
+namespace WMCDuplicateRemover.Tests
+{
+    [TestFixture]
+    public class ScheduledEventTests
+    {
+        IScheduledEvent _scheduledEvent;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _scheduledEvent = new MockScheduledEvent(
+                "The Daily Show", 
+                "Jon Stewart Discusses Current Events", 
+                false, 
+                new DateTime(2015, 3, 23, 10, 0, 0), 
+                false, 
+                new DateTime(2015, 3, 23, 10, 0, 0), 
+                Microsoft.MediaCenter.TV.Scheduling.ScheduleEventStates.WillOccur);
+
+            
+        }
+
+        [Test]
+        public void TestCreateScheduledEvent()
+        {
+            Assert.AreEqual("The Daily Show", _scheduledEvent.Title);
+            Assert.AreEqual("Jon Stewart Discusses Current Events", _scheduledEvent.Description);
+            Assert.False(_scheduledEvent.Partial);
+            Assert.AreEqual(new DateTime(2015, 3, 23, 10, 0, 0), _scheduledEvent.OriginalAirDate);
+            Assert.False(_scheduledEvent.Repeat);
+            Assert.AreEqual(new DateTime(2015, 3, 23, 10, 0, 0), _scheduledEvent.StartTime);
+            Assert.AreEqual(ScheduleEventStates.WillOccur, _scheduledEvent.State);
+        }
+    }
+}
