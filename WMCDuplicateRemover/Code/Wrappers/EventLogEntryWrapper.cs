de91cea9536c300eca48c3a31a644a6f74f4dcc2
Corey <corey@coreypon.com>
2015-04-26 16:21:51 -0500

    there was another bad test data since the api and WMC recorded the episode titles differently, also added some checks to make sure it won't mass cancel a show because the episode name is blank, and lastly added trimming on the compared strings just to be safe

--- WMCDuplicateRemover/Code/Wrappers/EventLogEntryWrapper.cs
+++ WMCDuplicateRemover/Code/Wrappers/EventLogEntryWrapper.cs
@@ -10,13 +10,17 @@ namespace WMCDuplicateRemover
     {
         public override bool FoundEventForRecording(String seriesName, String episodeName)
         {
-            var formattedEventData = String.Format("{0}: {1}", seriesName, episodeName).ToLower();
+            //Can't be cancelling shows we only have part of the information needed!
+            if (String.IsNullOrEmpty(seriesName) || String.IsNullOrEmpty(episodeName))
+                return false;
+
+            var formattedEventData = String.Format("{0}: {1}", seriesName, episodeName).Trim().ToLower();
 
             var eventLog = new EventLog("Media Center");
 
             foreach (EventLogEntry entry in eventLog.Entries)
             {
-                if (entry.Source == "Recording" && entry.Message.ToLower().Contains(formattedEventData))
+                if (entry.Source == "Recording" && entry.Message.Trim().ToLower().Contains(formattedEventData))
                     return true;
             }
 
